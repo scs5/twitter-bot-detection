b5a1730d877236b0914b6d3055e4a8a27dba812b
scsmit23 <scs5@clemson.edu>
2024-01-03 16:15:40 -0500

    Simplified preprocessing pipeline

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,25 +2,39 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 ORIGINAL_DATA_FN = './data/original_data.csv'
-TRAIN_DATA_FN = './data/train.csv'
-TEST_DATA_FN = './data/test.csv'
-
-
-def load_data():
-    df = pd.read_csv(ORIGINAL_DATA_FN, index_col=[0])
-    return df
+CURATED_DATA_FN = './data/curated_data.csv'
 
 
 def engineer_features(df):
+    # Extract hour from account creation timestamp
+    df['created_at'] = pd.to_datetime(df['created_at'])
+    df['hour_created'] = [date.hour for date in df['created_at']]
+    df = df.drop(columns='created_at')
+
+    # Extract length of description (bio)
+    df['description_length'] = [len(str(desc)) if not pd.isna(desc) 
+                                else 0 for desc in df['description']]
+    df = df.drop(columns='description')
+
+    # Remove usefless features
+    useless_features = ['id', 'profile_background_image_url', 'profile_image_url']
+    df = df.drop(columns=useless_features)
+
+    # One hot encode language
+    df = pd.get_dummies(df, columns=['lang'])
+
+    # Determine whether location is given
+    df['location'] = [True if loc!='unknown' else False for loc in df['location']]
+
+    # Extract length of screen name
+    df['name_length'] = [len(name) for name in df['screen_name']]
+    df = df.drop(columns='screen_name')
+
     return df
 
 
-def split_data(df):
-    train, test = train_test_split(df, test_size=0.2)
-    train.to_csv(TRAIN_DATA_FN, index=False)
-    test.to_csv(TEST_DATA_FN, index=False)
-
-
 if __name__ == '__main__':
-    df = load_data()
-    #split_data(df)
+    # Engineer features for train and test
+    df = pd.read_csv(ORIGINAL_DATA_FN, index_col=[0])
+    df = engineer_features(df)
+    df.to_csv(CURATED_DATA_FN, index=False)
